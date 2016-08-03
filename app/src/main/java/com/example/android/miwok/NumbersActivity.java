cec7d389cd3ab9e375ceed2746993f5badb151a1
aman <amanrocks11@gmail.com>
2016-08-04 00:11:46 +0530

    - Learnt Abstract classes, concrete classes and interfaces. - Learnt how to use an ArrayList - Learnt how to create a view dynamically using JAVA code. - View Recycling ( ListView, GridView)

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -2,6 +2,12 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -10,7 +16,21 @@ public class NumbersActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
 
-        String [] numbersEng = {"One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten"};
+        ArrayList<String> numbersEng = new ArrayList<String>();
+        numbersEng.add("One");
+        numbersEng.add("Two");
+        numbersEng.add("Three");
+        numbersEng.add("Four");
+        numbersEng.add("Five");
+        numbersEng.add("Six");
+        numbersEng.add("Seven");
+        numbersEng.add("Eight");
+        numbersEng.add("Nine");
+        numbersEng.add("Ten");
+
+        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, numbersEng);
+        ListView listView = (ListView) findViewById(R.id.list);
+        listView.setAdapter(itemsAdapter);
 
     }
 }
